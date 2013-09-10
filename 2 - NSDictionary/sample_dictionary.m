e81468745c052868932c270a035218fd26496b5b
adityanshastry <adityashastry1992@gmail.com>
2013-09-10 05:37:17 +0530

    Update sample_dictionary.m

--- 2 - NSDictionary/sample_dictionary.m
+++ 2 - NSDictionary/sample_dictionary.m
@@ -18,7 +18,7 @@ int main(int argc, const char* argv[])
 	NSLog(@"The dictionary items containing \"meter\":\n");
 	for(NSString* key in units)
 	{
-		if ([key rangeOfString:@"meter"].location != NSNotFound)
+		if ([key hasSuffix:@"meter"].location != NSNotFound)
 		{
 			NSLog(@"Unit: %@, Value: %@\n", key, [units objectForKey:key]);
 		}
